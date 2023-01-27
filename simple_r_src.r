fa4216c066a0cf541ac374e589fdfa07dc6f3520
Matt Burke <39552849+mattdburke@users.noreply.github.com>
2023-01-27 12:29:58 +0000

    both questions

--- simple_r_src.r
+++ simple_r_src.r
@@ -1,3 +1,44 @@
+p <- 0.2
+
+ret_x_mu <- 0.12
+ret_y_mu <- 0.05
+
+ret_x_sd <- 0.14
+ret_y_sd <- 0.08
+
+w <- seq(0, 1, 0.1)
+
+port_ret <- (ret_x_mu*w)+(ret_y_mu*(1-w))
+
+w_1 <- w
+w_2 <- (1-w)
+
+
+port_sd <- sqrt(
+    (w_1^2 * ret_x_sd^2) +
+    (w_2^2 * ret_y_sd^2) +
+    (2 * w_1 * w_2 * ret_x_sd * ret_y_sd * p)
+)
+
+plot(
+    port_sd, 
+    port_ret, 
+   # xlim=c(0.05, 0.22), 
+   # ylim=c(0.03, 0.2),
+    xlab = "Portfolio Volatility",
+    ylab = "Portfolio Expected Return")
+
+# sharpe ratio
+sr = (port_ret - 0.02) / port_sd
+plot(w, sr)
+
+
+
+
+
+
+
+
 p <- 0.1
 
 ret_x_mu <- 0.2
@@ -31,3 +72,5 @@ plot(
 # sharpe ratio
 sr = (port_ret - 0.02) / port_sd
 plot(w, sr)
+
+
